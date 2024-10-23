025497fa1cdc29afd59abac3a3923fb8f129edb8
katushenca <shutenko.katya@bk.ru>
2024-10-23 17:44:42 +0500

    add function template
    -adding base function templates

---
+++ imageHandler/imageHandler.py
@@ -0,0 +1,39 @@
+def check_for_min_quality(image):
+    """Возвращает True, если изображение подходит по качеству"""
+    pass
+
+
+def remove_transparency(image):
+    """Возвращает изображение без прозрачности"""
+    pass
+
+
+def identify_face(image):
+    """Возвращает True, если одно лицо на изображении"""
+    pass
+
+
+def turn_face(image):
+    """Возвращает изображение лица в нужной ориентации"""
+    pass
+
+
+def check_for_blur(image):
+    """Возвращает False, если изображение слишком размыто"""
+    pass
+
+
+def cut_quality(image):
+    """Возвращает изображение, урезанное по качеству,
+    если изображение слишком большое"""
+    pass
+
+
+def rename_image(image_name):
+    """Возвращает название изображения, соответствующее критериям"""
+    pass
+
+
+def save_image(image):
+    """Сохраняет изображение в папку"""
+    pass
