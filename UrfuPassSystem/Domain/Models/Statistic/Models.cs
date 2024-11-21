5d52fe639d7128a333cc4dad1218d67164817bd9
Sawer <danilch4024@gmail.com>
2024-11-21 17:42:10 +0500

    statistics

---
+++ UrfuPassSystem/Domain/Models/Statistic/Models.cs
@@ -0,0 +1,27 @@
+﻿using UrfuPassSystem.Domain.Enums;
+
+namespace UrfuPassSystem.Domain.Models.Statistic;
+
+/// <summary>Количество фотографий за промежуток времени.</summary>
+/// <param name="From">Начало промежутка времени.</param>
+/// <param name="To">Конец промежутка времени.</param>
+/// <param name="Success">Количество хороших.</param>
+/// <param name="Total">Общее количество.</param>
+public record TotalImageStatistic(DateTime From, DateTime To, int Success, int Total);
+
+/// <param name="ResultCode">Причина.</param>
+/// <param name="Count">Количество фотографий.</param>
+public record BadImageStatistic(ImageCheckResultCode ResultCode, int Count);
+
+/// <summary>Количество фотографий по каждой из причин за промежуток времени.</summary>
+/// <param name="From">Начало промежутка времени.</param>
+/// <param name="To">Конец промежутка времени.</param>
+/// <param name="Statistics">Список количества по причинам.</param>
+public record TotalBadImageStatistic(DateTime From, DateTime To, IReadOnlyList<BadImageStatistic> Statistics);
+
+/// <summary>Количество проверенных фотографий за промежуток времени.</summary>
+/// <param name="From">Начало промежутка времени.</param>
+/// <param name="To">Конец промежутка времени.</param>
+/// <param name="Checked">Количество проверенных.</param>
+/// <param name="Total">Общее количество.</param>
+public record ImageCheckStatistic(DateTime From, DateTime To, int Checked, int Total);
---
+++ UrfuPassSystem/Domain/Services/StatisticMaker/IStatisticMaker.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrfuPassSystem.Domain.Models.Statistic;
+
+namespace UrfuPassSystem.Domain.Services.StatisticMaker;
+
+/// <summary>Сервис генерации статистики.</summary>
+public interface IStatisticMaker
+{
+    /// <summary>Генерирует статистику фотографий.</summary>
+    /// <param name="dbContext">Контекст базы данных.</param>
+    /// <returns>Статистика.</returns>
+    public Task<TotalImageStatistic> MakeImageStatistic(ApplicationDbContext dbContext);
+
+    /// <summary>Генерирует статистику плохих фотографий.</summary>
+    /// <param name="dbContext">Контекст базы данных.</param>
+    /// <returns>Статистика.</returns>
+    public Task<TotalBadImageStatistic> MakeBadImageStatistic(ApplicationDbContext dbContext);
+
+    /// <summary>Генерирует статистику проверенных фотограрфий.</summary>
+    /// <param name="dbContext">Контекст базы данных.</param>
+    /// <returns>Статистика.</returns>
+    public Task<ImageCheckStatistic> MakeImageCheckStatistic(ApplicationDbContext dbContext);
+}
+
+public class StatisticMaker : IStatisticMaker
+{
+    public async Task<TotalImageStatistic> MakeImageStatistic(ApplicationDbContext dbContext)
+    {
+        var total = await dbContext.Images
+            .Where(i => !i.IsDeleted)
+            .CountAsync();
+        var success = await dbContext.Images
+            .Where(i => !i.IsDeleted)
+            .Include(i => i.Checks)
+            .Where(i => !i.Checks!.Any(c => !c.IsDeleted && !c.IsSuccess))
+            .CountAsync();
+        return new TotalImageStatistic(DateTime.UnixEpoch, DateTime.UtcNow, success, total);
+    }
+
+    public async Task<TotalBadImageStatistic> MakeBadImageStatistic(ApplicationDbContext dbContext)
+    {
+        var groups = await dbContext.Images
+            .Where(i => !i.IsDeleted)
+            .Include(i => i.Checks!.OrderBy(c => c.CheckTime))
+            .GroupBy(i => i.Checks!.Last(c => !c.IsDeleted).ResultCode)
+            .Where(i => i.Key != Enums.ImageCheckResultCode.Success)
+            .Select(g => new BadImageStatistic(g.Key, g.Count()))
+            .ToArrayAsync();
+        return new TotalBadImageStatistic(DateTime.UnixEpoch, DateTime.UtcNow, groups);
+    }
+
+    public async Task<ImageCheckStatistic> MakeImageCheckStatistic(ApplicationDbContext dbContext)
+    {
+        var total = await dbContext.Images
+            .Where(i => !i.IsDeleted)
+            .CountAsync();
+        var check = await dbContext.Images
+            .Where(i => !i.IsDeleted)
+            .Include(i => i.Checks)
+            .Where(i => i.Checks!.Any(c => !c.IsDeleted && !c.IsAuto))
+            .CountAsync();
+        return new ImageCheckStatistic(DateTime.UnixEpoch, DateTime.UtcNow, check, total);
+    }
+}
