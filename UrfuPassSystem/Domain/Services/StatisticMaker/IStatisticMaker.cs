4253f63008907b96a7a4f909425c9bb4c98652e3
Sawer <danilch4024@gmail.com>
2024-11-29 21:14:09 +0500

    statistics WIP (not works)

--- UrfuPassSystem/Domain/Services/StatisticMaker/IStatisticMaker.cs
+++ UrfuPassSystem/Domain/Services/StatisticMaker/IStatisticMaker.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using UrfuPassSystem.Domain.Models.Statistic;
+﻿using UrfuPassSystem.Domain.Models.Statistic;
 
 namespace UrfuPassSystem.Domain.Services.StatisticMaker;
 
@@ -20,45 +19,4 @@ public interface IStatisticMaker
     /// <param name="dbContext">Контекст базы данных.</param>
     /// <returns>Статистика.</returns>
     public Task<ImageCheckStatistic> MakeImageCheckStatistic(ApplicationDbContext dbContext);
-}
-
-public class StatisticMaker : IStatisticMaker
-{
-    public async Task<TotalImageStatistic> MakeImageStatistic(ApplicationDbContext dbContext)
-    {
-        var total = await dbContext.Images
-            .Where(i => !i.IsDeleted)
-            .CountAsync();
-        var success = await dbContext.Images
-            .Where(i => !i.IsDeleted)
-            .Include(i => i.Checks)
-            .Where(i => !i.Checks!.Any(c => !c.IsDeleted && !c.IsSuccess))
-            .CountAsync();
-        return new TotalImageStatistic(DateTime.UnixEpoch, DateTime.UtcNow, success, total);
-    }
-
-    public async Task<TotalBadImageStatistic> MakeBadImageStatistic(ApplicationDbContext dbContext)
-    {
-        var groups = await dbContext.Images
-            .Where(i => !i.IsDeleted)
-            .Include(i => i.Checks!.OrderBy(c => c.CheckTime))
-            .GroupBy(i => i.Checks!.Last(c => !c.IsDeleted).ResultCode)
-            .Where(i => i.Key != Enums.ImageCheckResultCode.Success)
-            .Select(g => new BadImageStatistic(g.Key, g.Count()))
-            .ToArrayAsync();
-        return new TotalBadImageStatistic(DateTime.UnixEpoch, DateTime.UtcNow, groups);
-    }
-
-    public async Task<ImageCheckStatistic> MakeImageCheckStatistic(ApplicationDbContext dbContext)
-    {
-        var total = await dbContext.Images
-            .Where(i => !i.IsDeleted)
-            .CountAsync();
-        var check = await dbContext.Images
-            .Where(i => !i.IsDeleted)
-            .Include(i => i.Checks)
-            .Where(i => i.Checks!.Any(c => !c.IsDeleted && !c.IsAuto))
-            .CountAsync();
-        return new ImageCheckStatistic(DateTime.UnixEpoch, DateTime.UtcNow, check, total);
-    }
 }
