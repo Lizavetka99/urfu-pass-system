237cae9c48b4d9bc4f818292486096d1a07abdc1
CepreNGT <malitevs@gmail.com>
2024-11-29 16:43:13 +0500

    Small fix in image rotator

--- imageHandler/image_rotator.py
+++ imageHandler/image_rotator.py
@@ -13,6 +13,7 @@ class ImageRotator:
         for y in range(height):
             right_cof += self.get_pixel_average(image[y, width - 4])
             left_cof += self.get_pixel_average(image[y, 3])
+        right_cof, left_cof, top_cof, bottom_cof = right_cof / height, left_cof / height, top_cof / width, bottom_cof / width
         min_cof = min(top_cof, left_cof, right_cof, bottom_cof)
         if (top_cof == min_cof):
             return cv2.rotate(image, cv2.ROTATE_180)
